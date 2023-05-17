29c9145ec098e0c72a6a65d4744cfcb8460a5989
Danyal Imran <dan.imran97@gmail.com>
2023-05-17 18:42:19 -0400

    working on hero section. So far So good

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,13 @@ const Hero = () => {
   return (
     <>
     <main>
-      <h1>Fitness is a state of being</h1>
-        <p>We are dedicated to guiding you on your journey to fitness.</p>
-        <button className='signup-button'>Sign Up</button>
-        <button className='trial-button'>'Trial' it out</button>
+      <div className='hero'>
+        <h1>Fitness is a state of being</h1>
+          <p>We are dedicated to guiding you on your journey to fitness.</p>
+          <button className='signup-button'>Sign Up</button>
+          <button className='trial-button'>'Trial' it out</button>
+      </div>
+
     </main>
     </>
   )
