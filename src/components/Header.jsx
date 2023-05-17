cf692b39a2c993bb5eea848d97de58e860a3ad90
Danyal Imran <dan.imran97@gmail.com>
2023-05-17 19:16:28 -0400

    fixed navbar to integrate with hero. BIG W

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,11 @@ const Header = () => {
       <nav>
         <ul className='header-nav'>
           <li className='nav-left'><h2>State of Fitness</h2></li>
+        
           <li className='nav-right'><a href='#'>Services</a></li>
           <li className='nav-right'><a href='#'>Facilities</a></li>
           <li className='nav-right'><a href='#'>Merch</a></li>
-          <li className='nav-right'><button className='nav-button'>Sign Up</button></li>
+          <li><button className='nav-button'>Sign Up</button></li>
         </ul>
       </nav>
     </>
