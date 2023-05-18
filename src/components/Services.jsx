68bbe8e2601e0dd01492ed2125f03bf505fe2f6e
Danyal Imran <dan.imran97@gmail.com>
2023-05-18 10:59:28 -0400

    working on styling

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,6 +7,12 @@ const Services = () => {
       <h2>Transform as you continue</h2>
       <p>We offer customized programs designed for each client to help them reach their next level. Whether that is weight loss, strength, toning, or conditioning.</p>
     </section>
+
+    <section className='services-grid'>
+      <div>
+        
+      </div>
+    </section>
     </>
   )
 }
